01f8df4207d04dc41b0a5c89224592c0ca041fb5
devcomfort <kimdonghyun026@gmail.com>
2024-05-09 14:15:34 +0000

    feat : Link  컴포넌트 추가

---
+++ src/routes/components/Link.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let href: string;
+	export let target: '_blank' | '_self' | '_top' | '_parent' = '_blank';
+</script>
+
+<a class="link" {href} {target}>
+	<slot></slot>
+</a>
