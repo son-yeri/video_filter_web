9f38791106df6502ff6983be1df3c3342079284a
devcomfort <kimdonghyun026@gmail.com>
2024-05-09 15:07:41 +0000

    feat : DaisyUI Table 컴포넌트 추가

---
+++ src/routes/components/Table.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	/** 열 제목들*/
+	export let row_titles: string[];
+	/** 행 데이터들 */
+	export let columns: (string | number)[][];
+</script>
+
+<div class="overflow-x-auto">
+	<table class="table">
+		<thead>
+			<tr>
+				{#each row_titles as title}
+					<th>{title}</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each columns as column}
+				<tr>
+					{#each column as cell}
+						<td>{cell}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
