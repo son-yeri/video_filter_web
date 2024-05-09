90e9198ae6ca5a8c0438ee61049b3f8458e63d35
devcomfort <kimdonghyun026@gmail.com>
2024-05-09 14:29:13 +0000

    design : 디자인 수정

--- src/routes/components/PerformanceMetric.svelte
+++ src/routes/components/PerformanceMetric.svelte
@@ -4,10 +4,10 @@
 	export let metric: Pick<PerformanceMetric, 'search_time' | 'filter_time' | 'all' | 'filtered'>;
 </script>
 
-<section class="flex flex-col gap-2 text-center">
+<div class="flex flex-col gap-4 border-2 border-black p-4 text-center">
 	<span class="text-xl font-bold"
 		>총 {(metric.all ?? []).length}개의 영상 중 {(metric.filtered ?? []).length}개 필터링됨</span
 	>
 	<span class="text-xl font-bold">검색 소요 시간: {metric.search_time} ms</span>
 	<span class="text-xl font-bold">필터 소요 시간: {metric.filter_time} ms</span>
-</section>
+</div>
