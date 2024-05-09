0860e8299679492541b590a381c6149218c183fe
devcomfort <kimdonghyun026@gmail.com>
2024-05-09 14:03:45 +0000

    feat : 레이아웃 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.pcss';
 </script>
 
-<slot></slot>
+<div data-theme="cmyk" class="g-4 flex h-screen w-screen flex-col items-center">
+	<!-- 컨테이너 -->
+	<div class="g-4 flex w-full max-w-screen-lg flex-col justify-center xl:max-w-screen-xl">
+		<slot></slot>
+	</div>
+</div>
